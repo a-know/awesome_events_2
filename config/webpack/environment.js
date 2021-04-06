22464138e6b4af9a821cb11e1e78a4384cea4c96
a-know <a.know.dev@gmail.com>
2021-04-06 22:46:15 +0900

    oops

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,7 @@
 const { environment } = require('@rails/webpacker')
 
 // https://qiita.com/1024xx4/items/56172d140d7208230e32
+// https://qiita.com/kazutosato/items/d47b7705ee545de4cb1a
 const webpack = require('webpack')
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
