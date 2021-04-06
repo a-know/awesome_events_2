dd8284a13c844a7beb191dafa9e15fd792a2657c
a-know <a.know.dev@gmail.com>
2021-04-06 22:32:00 +0900

    for Heroku

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
 const { environment } = require('@rails/webpacker')
 
+// https://qiita.com/1024xx4/items/56172d140d7208230e32
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+   $: 'jquery/src/jquery',
+   jQuery: 'jquery/src/jquery',
+   Popper: ['popper.js', 'default']
+}))
+
 module.exports = environment
