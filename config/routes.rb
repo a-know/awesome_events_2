a1bb0a118dbdcf931f5d2dd6a55369c5172918f6
a-know <a.know.dev@gmail.com>
2021-04-13 22:35:02 +0900

    Add routing OAuth callback

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   #get 'welcome/index'
   root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/auth/:provider/callback" => "sessions#create"
 end
