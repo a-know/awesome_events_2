e83537bdd6ed5f54ebb3933e59da410b775e0b68
a-know <a.know.dev@gmail.com>
2021-04-18 07:17:43 +0900

    Implement joining the Event

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,2 +1,16 @@
 class TicketsController < ApplicationController
+  def new
+    raise ActionController::RoutingError, "Accessed TicketsController#new in logged-in state"
+  end
+
+  def create
+    event = Event.find(params[:event_id])
+    @ticket = current_user.tickets.build do |t|
+      t.event = event
+      t.comment = params[:ticket][:comment]
+    end
+    if @ticket.save
+      redirect_to event, notice: "You joined this Event."
+    end
+  end
 end
