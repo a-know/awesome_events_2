ff85d5f2d3e08798ccef58a7a00e90bb86e8ba65
a-know <a.know.dev@gmail.com>
2021-04-19 07:10:40 +0900

    Implement retirement

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+
+  resource :retirements
+
   #get 'welcome/index'
   root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
