e53b67a0770027a4fce3e93feaf319f79fe2ac84
a-know <a.know.dev@gmail.com>
2021-04-23 06:48:08 +0900

    Event's factory_bot definition

--- test/factories/events.rb
+++ test/factories/events.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :event do
-    
+    owner
+    sequence(:name) { |i| "Event Name #{i}" }
+    sequence(:place) { |i| "Event Location #{i}" }
+    sequence(:content) { |i| "Event Content #{i}" }
+    start_at { rand(1..30).days.from_now }
+    end_at { start_at + rand(1..30).hours }
   end
 end
