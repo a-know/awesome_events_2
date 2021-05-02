a5bb75fc4ad3df18d7192951fcd065edb86e07d3
a-know <a.know.dev@gmail.com>
2021-05-02 23:12:13 +0900

    Add E2E test for shown or not-shown event on / page

--- test/system/welcomes_test.rb
+++ test/system/welcomes_test.rb
@@ -6,4 +6,15 @@ class WelcomesTest < ApplicationSystemTestCase
   
     assert_selector "h1", text: "Events List"
   end
+
+  test "future event is shown but past event is not shown when visit /" do
+    future_event = FactoryBot.create(:event, start_at: Time.zone.now + 3.day)
+    past_event = FactoryBot.create(:event, start_at: Time.zone.now + 1.day)
+
+    travel_to Time.zone.now + 2.day do
+      visit root_url
+      assert_selector "h5", text: future_event.name
+      assert_no_selector "h5", text: past_event.name
+    end
+  end
 end
